ab53549131bfd566fe5cae23e5a23a0d8cad3841
Parth Panchal <pr930523@dal.ca>
2020-02-06 23:05:22 -0400

    "Added comment"

--- src/main/java/com/group3/groupmanager/GroupmanagerApplication.java
+++ src/main/java/com/group3/groupmanager/GroupmanagerApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.group3.ApplicationContext.ConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GroupmanagerApplication {
-
+//hello comment
 	public static void main(String[] args) {
 	ConfigurableApplicationContext context = 	SpringApplication.run(GroupmanagerApplication.class, args);
 		new ConfigApplicationContext().setApplicationContext(context);
